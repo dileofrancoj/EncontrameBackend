1e66d6f286fec30620b6ba44e7640e236047b467
Franco Di Leo <dielo.francoj@gmail.com>
2020-12-08 16:34:20 -0300

    Creacion de usuario

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,16 @@
 const User = require("./../models/Users");
+const { hash, unhash } = require("./../utils/bcrypt");
 const create = async (req, res) => {
   const { email, password } = req.body;
   let user = await User.findOne({ email });
   if (user)
     return res.status(400).json({ ok: false, msg: "El correo ya existe" });
   user = new User(req.body);
-  user.password = "passwordEncriptada";
+  user.password = hash(password);
   await user.save();
-  res.status(201).json({ ok: true, msg: "Usuario creado correctamente" });
+  res
+    .status(201)
+    .json({ ok: true, msg: "Usuario creado correctamente", _id: user._id });
 };
 
 module.exports = { create };
